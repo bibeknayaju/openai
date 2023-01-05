4a6612c97b5af2be7f353fbb1d368d4c39c3e0d9
Bibek Nayaju <bibeknayaju@github.com>
2023-01-06 00:14:08 +0545

    update server link

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bot's response
-  const response = await fetch("http://localhost:5001/", {
+  const response = await fetch("https://openai-uw8q.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
